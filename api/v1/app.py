d0138924002f458d105fbe6cb01251fb38f09fe0
Nyangolo <96448943+Nyangolo@users.noreply.github.com>
2023-11-04 00:49:47 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,23 +1,50 @@
 #!/usr/bin/python3
-"""returns api status code"""
-
-from api.v1.views import app_views
-from flask import Flask
-from models import storage
-from os import getenv
+'''Contains a Flask web application API.
+'''
+import os
+from flask import Flask, jsonify
+from flask_cors import CORS
 import api/v1/views/index.py
+from models import storage
+from api.v1.views import app_views
+
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix="/api/v1")
+'''The Flask web application instance.'''
+app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+CORS(app, resources={'/*': {'origins': app_host}})
 
 
 @app.teardown_appcontext
-def close_session(session):
-    """close a session"""
+def teardown_flask(exception):
+    '''The Flask app/request context end event listener.'''
+    # print(exception)
     storage.close()
 
 
+@app.errorhandler(404)
+def error_404(error):
+    '''Handles the 404 HTTP error code.'''
+    return jsonify(error='Not found'), 404
+
+
+@app.errorhandler(400)
+def error_400(error):
+    '''Handles the 400 HTTP error code.'''
+    msg = 'Bad request'
+    if isinstance(error, Exception) and hasattr(error, 'description'):
+        msg = error.description
+    return jsonify(error=msg), 400
+
+
 if __name__ == '__main__':
-    host = getenv('HBNB_API_HOST') if getenv('HBNB_API_HOST') else '0.0.0.0'
-    port = getenv('HBNB_API_PORT') if getenv('HBNB_API_PORT') else 5000
-    app.run(port=port, host=host, threaded=True)
+    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+    app.run(
+        host=app_host,
+        port=app_port,
+        threaded=True
+    )
